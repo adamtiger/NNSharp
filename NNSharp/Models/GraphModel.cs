8aa5f9fd597ca4bd8eb5af56c6d0945a16840aca
Adam Budai <budai8adam@gmail.com>
2017-05-29 00:49:55 +0200

    IModel was added to GraphModel as well.

--- NNSharp/Models/GraphModel.cs
+++ NNSharp/Models/GraphModel.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace NNSharp.Models
 {
-    class GraphModel
+    class GraphModel : IModel
     {
+        public IModelData GetSummary()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
