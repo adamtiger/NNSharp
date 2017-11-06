51337a18063d01a2e25a0e350be60b9990680139
Adam Budai <budai8adam@gmail.com>
2017-11-06 14:26:45 +0100

    NNSharp was set to x64 due to tensorflow dll.

---
+++ NNSharp/BindingCore/NativeFunctions_Tensors.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using NNSharp.Properties;
+
+namespace NNSharp.BindingCore
+{
+    static public partial class NativeFunctions
+    {
+        const string CoreDll = @"D:\ArtificialIntelligence\MachineLearning\git\NNSharp\cppcore\generated\Debug\cppcore.dll";
+
+        [DllImport(CoreDll, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern IntPtr create_tensor_integer(int size);
+
+        [DllImport(CoreDll, CallingConvention = CallingConvention.Cdecl, ExactSpelling = true)]
+        public static extern int tensor_integer_get(IntPtr tensor_in, int idx);
+    }
+}
